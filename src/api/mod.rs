3ecaaabfe1176341041def1b753bd9a3c257a5df
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-11-13 15:36:37 +0100

    [UPD] api/mod.rs

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,4 +1,13 @@
+const DISCORD_API_BASE_URL: &str = "https://discord.com/api/v10";
+
 pub mod channel;
+pub mod emoji;
 pub mod guild;
 pub mod message;
 pub mod user;
+
+pub use channel::get_channel::get_channel;
+pub use emoji::get_guild_emojis::get_guild_emojis;
+pub use guild::get_guild_channels::get_guild_channels;
+pub use message::{create_message::create_message, get_channel_messages::get_channel_messages};
+pub use user::get_current_user_guilds::get_current_user_guilds;
