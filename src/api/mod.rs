49de02fb2775c1e8e5b274ec913640f68cfd5b1a
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-11-14 11:33:07 +0100

    [UPD] api/mod.rs

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -6,8 +6,8 @@ pub mod guild;
 pub mod message;
 pub mod user;
 
-pub use channel::get_channel::get_channel;
-pub use emoji::get_guild_emojis::get_guild_emojis;
-pub use guild::get_guild_channels::get_guild_channels;
-pub use message::{create_message::create_message, get_channel_messages::get_channel_messages};
-pub use user::get_current_user_guilds::get_current_user_guilds;
+pub use channel::get_channel;
+pub use emoji::get_guild_emojis;
+pub use guild::get_guild_channels;
+pub use message::{create_message, get_channel_messages};
+pub use user::get_current_user_guilds;
