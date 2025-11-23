b108db89872110c6b25d4ef1e4cc6dc304438bbe
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-11-23 13:34:57 +0100

    [UPDATE] guild.rs
    Add GuildMember

--- src/api/guild.rs
+++ src/api/guild.rs
@@ -1,5 +1,13 @@
 use serde::{Deserialize, Serialize};
 
+use crate::api::User;
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct GuildMember {
+    pub user: User,
+    pub roles: Vec<String>,
+}
+
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Guild {
     pub id: String,
